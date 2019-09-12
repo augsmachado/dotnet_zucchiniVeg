14c490f60d9c75b273014f01f8dd0cbdc0e1c6ff
Augusto Machado <augustomachadoramos@gmail.com>
2019-09-12 14:35:10 -0300

    Create project

---
+++ Pages/Request.cstml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace abobrinhaVeg.Pages
+{
+    public class RequestModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
